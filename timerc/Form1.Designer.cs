f507087bccdaa89656acabf06c64c87f25028f7c
K-R-I-K <smyrf200323@gmail.com>
2021-10-23 23:13:27 +0300

    Add project files.

---
+++ timerc/Form1.Designer.cs
@@ -0,0 +1,189 @@
+﻿
+namespace timerc
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.listOfTimers = new System.Windows.Forms.ListBox();
+            this.time = new System.Windows.Forms.DateTimePicker();
+            this.buttonAdd = new System.Windows.Forms.Button();
+            this.timerMode = new System.Windows.Forms.RadioButton();
+            this.alarmMode = new System.Windows.Forms.RadioButton();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.groupBox2 = new System.Windows.Forms.GroupBox();
+            this.groupBox3 = new System.Windows.Forms.GroupBox();
+            this.deleteButton = new System.Windows.Forms.Button();
+            this.groupBox1.SuspendLayout();
+            this.groupBox2.SuspendLayout();
+            this.groupBox3.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // timer1
+            // 
+            this.timer1.Interval = 1000;
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+            // 
+            // listOfTimers
+            // 
+            this.listOfTimers.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.listOfTimers.FormattingEnabled = true;
+            this.listOfTimers.ItemHeight = 16;
+            this.listOfTimers.Location = new System.Drawing.Point(6, 120);
+            this.listOfTimers.Name = "listOfTimers";
+            this.listOfTimers.Size = new System.Drawing.Size(606, 212);
+            this.listOfTimers.TabIndex = 0;
+            // 
+            // time
+            // 
+            this.time.Anchor = System.Windows.Forms.AnchorStyles.Left;
+            this.time.Font = new System.Drawing.Font("Microsoft Sans Serif", 18F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.time.Format = System.Windows.Forms.DateTimePickerFormat.Time;
+            this.time.Location = new System.Drawing.Point(9, 13);
+            this.time.Name = "time";
+            this.time.Size = new System.Drawing.Size(167, 41);
+            this.time.TabIndex = 1;
+            this.time.Value = new System.DateTime(2021, 10, 23, 0, 0, 0, 0);
+            // 
+            // buttonAdd
+            // 
+            this.buttonAdd.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Left | System.Windows.Forms.AnchorStyles.Right)));
+            this.buttonAdd.Location = new System.Drawing.Point(182, 13);
+            this.buttonAdd.Name = "buttonAdd";
+            this.buttonAdd.Size = new System.Drawing.Size(146, 41);
+            this.buttonAdd.TabIndex = 5;
+            this.buttonAdd.Text = "Add";
+            this.buttonAdd.UseVisualStyleBackColor = true;
+            this.buttonAdd.Click += new System.EventHandler(this.buttonAdd_Click);
+            // 
+            // timerMode
+            // 
+            this.timerMode.AutoSize = true;
+            this.timerMode.Checked = true;
+            this.timerMode.Location = new System.Drawing.Point(6, 10);
+            this.timerMode.Name = "timerMode";
+            this.timerMode.Size = new System.Drawing.Size(65, 21);
+            this.timerMode.TabIndex = 6;
+            this.timerMode.TabStop = true;
+            this.timerMode.Text = "Timer";
+            this.timerMode.UseVisualStyleBackColor = true;
+            this.timerMode.CheckedChanged += new System.EventHandler(this.timerMode_CheckedChanged);
+            // 
+            // alarmMode
+            // 
+            this.alarmMode.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.alarmMode.AutoSize = true;
+            this.alarmMode.Location = new System.Drawing.Point(530, 10);
+            this.alarmMode.Name = "alarmMode";
+            this.alarmMode.Size = new System.Drawing.Size(65, 21);
+            this.alarmMode.TabIndex = 7;
+            this.alarmMode.Text = "Alarm";
+            this.alarmMode.UseVisualStyleBackColor = true;
+            this.alarmMode.CheckedChanged += new System.EventHandler(this.alarmMode_CheckedChanged);
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.groupBox1.Controls.Add(this.deleteButton);
+            this.groupBox1.Controls.Add(this.time);
+            this.groupBox1.Controls.Add(this.buttonAdd);
+            this.groupBox1.Location = new System.Drawing.Point(-1, 34);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(596, 60);
+            this.groupBox1.TabIndex = 8;
+            this.groupBox1.TabStop = false;
+            // 
+            // groupBox2
+            // 
+            this.groupBox2.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.groupBox2.Controls.Add(this.groupBox1);
+            this.groupBox2.Controls.Add(this.alarmMode);
+            this.groupBox2.Controls.Add(this.timerMode);
+            this.groupBox2.Location = new System.Drawing.Point(11, 10);
+            this.groupBox2.Name = "groupBox2";
+            this.groupBox2.Size = new System.Drawing.Size(601, 104);
+            this.groupBox2.TabIndex = 9;
+            this.groupBox2.TabStop = false;
+            // 
+            // groupBox3
+            // 
+            this.groupBox3.Controls.Add(this.groupBox2);
+            this.groupBox3.Controls.Add(this.listOfTimers);
+            this.groupBox3.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.groupBox3.Location = new System.Drawing.Point(0, 0);
+            this.groupBox3.Name = "groupBox3";
+            this.groupBox3.Size = new System.Drawing.Size(618, 338);
+            this.groupBox3.TabIndex = 10;
+            this.groupBox3.TabStop = false;
+            // 
+            // deleteButton
+            // 
+            this.deleteButton.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Left | System.Windows.Forms.AnchorStyles.Right)));
+            this.deleteButton.Location = new System.Drawing.Point(444, 13);
+            this.deleteButton.Name = "deleteButton";
+            this.deleteButton.Size = new System.Drawing.Size(146, 41);
+            this.deleteButton.TabIndex = 6;
+            this.deleteButton.Text = "Delete";
+            this.deleteButton.UseVisualStyleBackColor = true;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(618, 338);
+            this.Controls.Add(this.groupBox3);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox2.ResumeLayout(false);
+            this.groupBox2.PerformLayout();
+            this.groupBox3.ResumeLayout(false);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Timer timer1;
+        private System.Windows.Forms.ListBox listOfTimers;
+        private System.Windows.Forms.DateTimePicker time;
+        private System.Windows.Forms.Button buttonAdd;
+        private System.Windows.Forms.RadioButton timerMode;
+        private System.Windows.Forms.RadioButton alarmMode;
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.GroupBox groupBox2;
+        private System.Windows.Forms.GroupBox groupBox3;
+        private System.Windows.Forms.Button deleteButton;
+    }
+}
+
---
+++ timerc/Form1.cs
@@ -0,0 +1,123 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Media;
+using System.Windows.Forms;
+
+namespace timerc
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+            timer1.Start();
+        }
+        
+        SoundPlayer sound = new SoundPlayer(@"C:\Users\Kyryl\FKNK_1\2\timerc\timerc\Jenix-Catch Fire.wav");
+        List<int> Timers = new List<int>();
+        List<DateTime> Alarm = new List<DateTime>();
+        List<bool> typeOfTimer = new List<bool>();
+        string tmpstring;
+        //int tmptime, tmpday;
+        //Tuple<int, int> tmppair;
+        DateTime curTime;
+        private string IntToStringTime(int time)
+        {
+            int hours = (time - (time % (3600))) / (3600);
+            int minutes = (time - time % 60) / 60 - hours * 60;
+            int seconds = time - hours * 3600 - minutes * 60;
+            return String.Format("{0:00}:{1:00}:{2:00}", hours, minutes, seconds);
+        }
+
+        int alarmCount, timerCount;
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            if (listOfTimers.Items.Count != 0)
+            {
+                alarmCount = 0;
+                timerCount = 0;
+                for (int i = 0; i < listOfTimers.Items.Count; ++i)
+                {
+                    if (typeOfTimer[i])
+                    {
+                        if (Timers[timerCount] == 0)
+                        {
+                            listOfTimers.Items.RemoveAt(i);
+                            Timers.RemoveAt(timerCount);
+                            typeOfTimer.RemoveAt(i);
+                            sound.Play();
+                            MessageBox.Show("Time of " + (timerCount + 1) + " Timer is out");
+                            sound.Stop();
+                            --i;
+                        }
+                        else
+                        {
+                            --Timers[timerCount];
+                            listOfTimers.Items[i] = IntToStringTime(Timers[timerCount]);
+                            ++timerCount;
+                        }
+                    }
+                    else
+                    {
+                        curTime = DateTime.Now;
+                        if (Alarm[alarmCount] <= curTime)
+                        {
+                            listOfTimers.Items.RemoveAt(i);
+                            Alarm.RemoveAt(alarmCount);
+                            typeOfTimer.RemoveAt(i);
+                            sound.Play();
+                            MessageBox.Show("Time of " + (alarmCount + 1) + " Alarm is out");
+                            sound.Stop();
+                            --i;
+                        }
+                        else
+                            ++alarmCount;
+                    }
+                }
+            }
+        }
+
+        private void buttonAdd_Click(object sender, EventArgs e)
+        {
+            if (timerMode.Checked)
+            {
+                typeOfTimer.Add(true);
+                tmpstring = time.Value.TimeOfDay.ToString();
+                listOfTimers.Items.Add(tmpstring);
+                Timers.Add(3600 * time.Value.Hour + 60 * time.Value.Minute + time.Value.Second);
+            }
+            else
+            {
+                curTime = DateTime.Now;
+                if (time.Value <= curTime)
+                {
+                    MessageBox.Show("enter time < current time");
+                }
+                else
+                {
+                    typeOfTimer.Add(false);
+                    tmpstring = time.Value.ToString();
+                    listOfTimers.Items.Add(tmpstring);
+                    Alarm.Add(time.Value);
+                }
+            }
+        }
+
+        private void timerMode_CheckedChanged(object sender, EventArgs e)
+        {
+            time.Format = DateTimePickerFormat.Time;
+        }
+
+        private void alarmMode_CheckedChanged(object sender, EventArgs e)
+        {
+            time.Format = DateTimePickerFormat.Custom;
+            time.CustomFormat = "MM/dd/yyyy HH:mm:ss";
+        }
+    }
+}
